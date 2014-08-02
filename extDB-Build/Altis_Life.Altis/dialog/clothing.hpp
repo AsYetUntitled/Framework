75ba2e2f0389598db9cd1a9674487cdbb97b648e
Dillon Modine-Thuen <itsyuka101@gmail.com>
2014-08-01 20:13:59 -0600

    Clothing UI fixes
    And removed a file that wasn't being used

--- extDB-Build/Altis_Life.Altis/dialog/clothing.hpp
+++ extDB-Build/Altis_Life.Altis/dialog/clothing.hpp
@@ -9,7 +9,7 @@ class Life_Clothing {
 		class Life_RscTitleBackground:Life_RscText {
 			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])", "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])", "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])", "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.7])"};
 			idc = -1;
-			x = -0.30;
+			x = (1 / 250 / (safezoneW / safezoneH));
 			y = 0;
 			w = 0.318;
 			h = (1 / 25);
@@ -18,7 +18,7 @@ class Life_Clothing {
 		class MainBackground:Life_RscText {
 			colorBackground[] = {0, 0, 0, 0.7};
 			idc = -1;
-			x = -0.30;
+			x = (1 / 250 / (safezoneW / safezoneH));
 			y = 0 + (11 / 250);
 			w = 0.318;
 			h = 0.5 - (22 / 250);
@@ -32,7 +32,7 @@ class Life_Clothing {
 			colorBackground[] = {0, 0, 0, 0};
 			idc = 3103;
 			text = "";
-			x = -0.30;
+			x = (1 / 250 / (safezoneW / safezoneH));
 			y = 0;
 			w = 0.6;
 			h = (1 / 25);
@@ -45,8 +45,10 @@ class Life_Clothing {
 			sizeEx = 0.035;
 			onLBSelChanged = "[_this] call life_fnc_changeClothes;";
 			
-			x = -0.29; y = 0.05;
-			w = 0.3; h = 0.35;
+			x = 0.01 + (1 / 250 / (safezoneW / safezoneH));
+			y = 0.05;
+			w = 0.3;
+			h = 0.35;
 		};
 		
 		class CloseButtonKey : Life_RscButtonMenu 
@@ -54,7 +56,7 @@ class Life_Clothing {
 			idc = -1;
 			text = "$STR_Global_Close";
 			onButtonClick = "closeDialog 0;";
-			x = -0.14;
+			x = 0.161 + (1 / 250 / (safezoneW / safezoneH));
 			y = 0.54 - (1 / 25);
 			w = (6.25 / 40);
 			h = (1 / 25);
@@ -65,7 +67,7 @@ class Life_Clothing {
 			idc = -1;
 			text = "$STR_Global_Buy";
 			onButtonClick = "[] call life_fnc_buyClothes;";
-			x = -0.30;
+			x = (1 / 250 / (safezoneW / safezoneH));
 			y = 0.54 - (1 / 25);
 			w = (6.25 / 40);
 			h = (1 / 25);
@@ -76,8 +78,10 @@ class Life_Clothing {
 			idc = 3105;
 			colorBackground[] = {0,0,0,0.7};
 			onLBSelChanged  = "_this call life_fnc_clothingFilter";
-			x = -0.30; y = 0.5 - (1 / 25);
-			w = 0.318; h = 0.035;
+			x = (1 / 250 / (safezoneW / safezoneH));
+			y = 0.5 - (1 / 25);
+			w = 0.318;
+			h = 0.035;
 		};
 		
 		class PriceTag : Life_RscStructuredText
@@ -86,7 +90,7 @@ class Life_Clothing {
 			text = "";
 			sizeEx = 0.035;
 			
-			x = -0.30;
+			x = (1 / 250 / (safezoneW / safezoneH));
 			y = 0.45 - (1 / 25);
 			w = 0.2;
 			h = 0.2;
@@ -98,7 +102,7 @@ class Life_Clothing {
 			text = "";
 			sizeEx = 0.035;
 			
-			x = -0.15;
+			x = 0.15 + (1 / 250 / (safezoneW / safezoneH));
 			y = 0.45 - (1 / 25);
 			w = 0.2;
 			h = 0.2;
