5f9c0b56477d1c7a05142fe00212fafcc22eba4e
Dillon Modine-Thuen <itsyuka101@gmail.com>
2015-11-16 23:36:57 -0200

    HUD: New in-game user interface.

--- Altis_Life.Altis/dialog/ui.hpp
+++ Altis_Life.Altis/dialog/ui.hpp
@@ -1,98 +1,129 @@
-	class playerHUD
-   	{
-		idd=-1;
-		movingEnable=0;
-	  	fadein=0;
-		duration = 99999999999999999999999999999999999999999999;
-	  	fadeout=0;
-		name="playerHUD";
-		onLoad="uiNamespace setVariable ['playerHUD',_this select 0]";
-		objects[]={};
-		
-		class controlsBackground 
-		{
-			class foodHIcon : life_RscPicture 
-			{
-			
-				idc = -1;
-				text = "icons\food.paa";
-				x = safeZoneX+safeZoneW-0.115; y = safeZoneY+safeZoneH-0.54;
-				w = 0.03; h = 0.04;
-			};
-			
-			class waterHIcon : life_RscPicture 
-			{
-			
-				idc = -1;
-				text = "icons\water.paa";
-				x = safeZoneX+safeZoneW-0.12; y = safeZoneY+safeZoneH-0.50;
-				w = 0.04; h = 0.04;
-			};
-			
-			class healthHIcon : life_RscPicture
-			{
-				
-				idc = -1;
-				text = "icons\health.paa";
-				x = safeZoneX+safeZoneW-0.11; y = safeZoneY+safeZoneH-0.445;
-				w = 0.02; h = 0.03;
-			};
-		};
-		
-		class controls
-		{
-			class foodtext
-			{
-				type=0;
-				idc=23500;
-				style=0;
-				x=-1;
-				y=-1;
-				w=0.3;
-				h=0.05;
-				sizeEx=0.03;
-				size=1;
-				font="PuristaSemibold";
-				colorBackground[]={0,0,0,0};
-				colorText[] = { 1 , 1 , 1 , 1 };
-				shadow=true;
-				text="";
-			};
-			
-			class watertext
-			{
-				type=0;
-				idc=23510;
-				style=0;
-				x=-1;
-				y=-1;
-				w=0.3;
-				h=0.05;
-				sizeEx=0.03;
-				size=1;
-				font="PuristaSemibold";
-				colorBackground[]={0,0,0,0};
-				colorText[] = { 1 , 1 , 1 , 1 };
-				shadow=true;
-				text="";
-			};
-			
-			class healthtext
-			{
-				type=0;
-				idc=23515;
-				style=0;
-				x=-1;
-				y=-1;
-				w=0.3;
-				h=0.05;
-				sizeEx=0.03;
-				size=1;
-				font="PuristaSemibold";
-				colorBackground[]={0,0,0,0};
-				colorText[] = { 1 , 1 , 1 , 1 };
-				shadow=true;
-				text="";
-			};
-		};   
- 	};
+#define INFINITE 1e+1000
+#define IDC_LIFE_BAR_FOOD 2200
+#define IDC_LIFE_BAR_WATER 2201
+#define IDC_LIFE_BAR_HEALTH 2202
+#define IDC_LIFE_PICTURE_FOOD 1200
+#define IDC_LIFE_PICTURE_WATER 1201
+#define IDC_LIFE_PICTURE_HEALTH 1202
+#define IDC_LIFE_FOOD_TEXT 1000
+#define IDC_LIFE_WATER_TEXT 1001
+#define IDC_LIFE_HEALTH_TEXT 1002
+/*
+	Name: Dillon "Itsyuka" Modine-Thuen
+	File: ui.hpp
+*/
+
+class playerHUD {
+	idd = -1;
+	duration = INFINITE;
+	movingEnable = 0;
+	fadein = 0;
+	fadeout = 0;
+	name = "playerHUD";
+	onLoad = "uiNamespace setVariable ['playerHUD',_this select 0]";
+	objects[] = {};
+	controls[] = {
+		LIFE_UI_BACK,
+		LIFE_BAR_FOOD,
+		LIFE_BAR_WATER,
+		LIFE_BAR_HEALTH,
+		LIFE_PICTURE_FOOD,
+		LIFE_PICTURE_WATER,
+		LIFE_PICTURE_HEALTH,
+		LIFE_FOOD_TEXT,
+		LIFE_WATER_TEXT,
+		LIFE_HEALTH_TEXT
+	};
+
+	/* Background */
+	class LIFE_UI_BACK: Life_RscBackground
+	{
+		colorBackground[] = {0,0,0,0.5};
+		x = 0.917656 * safezoneW + safezoneX;
+		y = 0.819 * safezoneH + safezoneY;
+		w = 0.0825 * safezoneW;
+		h = 0.11 * safezoneH;
+	};
+
+	/* Progress Bars */
+	class LIFE_BAR_FOOD: Life_RscProgress
+	{
+		idc = IDC_LIFE_BAR_FOOD;
+		x = 0.938281 * safezoneW + safezoneX;
+		y = 0.83 * safezoneH + safezoneY;
+		w = 0.061875 * safezoneW;
+		h = 0.022 * safezoneH;
+	};
+	class LIFE_BAR_WATER: Life_RscProgress
+	{
+		idc = IDC_LIFE_BAR_WATER;
+		x = 0.938281 * safezoneW + safezoneX;
+		y = 0.863 * safezoneH + safezoneY;
+		w = 0.061875 * safezoneW;
+		h = 0.022 * safezoneH;
+	};
+	class LIFE_BAR_HEALTH: Life_RscProgress
+	{
+		idc = 2202;
+		x = 0.938281 * safezoneW + safezoneX;
+		y = 0.896 * safezoneH + safezoneY;
+		w = 0.061875 * safezoneW;
+		h = 0.022 * safezoneH;
+	};
+
+	/* Text */
+
+	class LIFE_FOOD_TEXT: Life_RscText
+	{
+		idc = IDC_LIFE_FOOD_TEXT;
+		text = "";
+		x = 0.958906 * safezoneW + safezoneX;
+		y = 0.8344 * safezoneH + safezoneY;
+		w = 0.020625 * safezoneW;
+		h = 0.011 * safezoneH;
+	};
+	class LIFE_WATER_TEXT: Life_RscText
+	{
+		idc = IDC_LIFE_WATER_TEXT;
+		text = "";
+		x = 0.958905 * safezoneW + safezoneX;
+		y = 0.8674 * safezoneH + safezoneY;
+		w = 0.020625 * safezoneW;
+		h = 0.011 * safezoneH;
+	};
+	class LIFE_HEALTH_TEXT: Life_RscText
+	{
+		idc = IDC_LIFE_HEALTH_TEXT;
+		text = "";
+		x = 0.958906 * safezoneW + safezoneX;
+		y = 0.9004 * safezoneH + safezoneY;
+		w = 0.020625 * safezoneW;
+		h = 0.011 * safezoneH;
+	};
+
+	/* Icons */
+	class LIFE_PICTURE_FOOD: life_RscPicture
+	{
+		idc = IDC_LIFE_PICTURE_FOOD;
+		text = "icons\food.paa";
+		x = 0.922813 * safezoneW + safezoneX;
+		y = 0.83 * safezoneH + safezoneY;
+		w = 0.03; h = 0.04;
+	};
+	class LIFE_PICTURE_WATER: life_RscPicture
+	{
+		idc = IDC_LIFE_PICTURE_WATER;
+		text = "icons\water.paa";
+		x = 0.922813 * safezoneW + safezoneX;
+		y = 0.863 * safezoneH + safezoneY;
+		w = 0.03; h = 0.04;
+	};
+	class LIFE_PICTURE_HEALTH: life_RscPicture
+	{
+		idc = IDC_LIFE_PICTURE_HEALTH;
+		text = "icons\health.paa";
+		x = 0.922813 * safezoneW + safezoneX;
+		y = 0.896 * safezoneH + safezoneY;
+		w = 0.03; h = 0.04;
+	};
+};
