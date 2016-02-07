f8948b25c224b535c521698c3daa9c58b38e6d3a
Daniel Stuart <daniel.stuart14@gmail.com>
2016-02-07 19:27:04 -0200

    Config: Master: Clean

--- Altis_Life.Altis/Config_Master.hpp
+++ Altis_Life.Altis/Config_Master.hpp
@@ -72,55 +72,57 @@ class Life_Settings {
     /* Car-shop Settings */
     vehicleShop_rentalOnly[] = { "B_MRAP_01_hmg_F", "B_G_Offroad_01_armed_F" };
 
-    crimes[] = {
-      {"STR_Crime_187V","650","187V"},
-      {"STR_Crime_187","2000","187"},
-      {"STR_Crime_901","450","901"},
-      {"STR_Crime_215","200","215"},
-      {"STR_Crime_213","1000","213"},
-      {"STR_Crime_211","100","211"},
-      {"STR_Crime_207","350","207"},
-      {"STR_Crime_207A","200","207A"},
-  		{"STR_Crime_390","1500","390"},
-      {"STR_Crime_487","150","487"},
-      {"STR_Crime_488","70","488"},
-      {"STR_Crime_480","100","480"},
-      {"STR_Crime_481","100","481"},
-      {"STR_Crime_482","500","482"},
-      {"STR_Crime_483","950","483"},
-      {"STR_Crime_459","650","459"},
-      {"STR_Crime_666","200","666"},
-      {"STR_Crime_667","4500","667"},
-      {"STR_Crime_668","1500","668"},
-      {"STR_Crime_1","250","1"},
-  		{"STR_Crime_2","200","2"},
-  		{"STR_Crime_3","150","3"},
-  		{"STR_Crime_4","250","4"},
-  		{"STR_Crime_5","100","5"},
-  		{"STR_Crime_6","80","6"},
-  		{"STR_Crime_7","150","7"},
-  		{"STR_Crime_8","5000","8"},
-  		{"STR_Crime_9","5000","9"},
-  		{"STR_Crime_10","15000","10"},
-  		{"STR_Crime_11","10000","11"},
-  		{"STR_Crime_12","2500","12"},
-  		{"STR_Crime_13","1500","13"},
-  		{"STR_Crime_14","500","14"},
-  		{"STR_Crime_15","2500","15"},
-  		{"STR_Crime_16","1500","16"},
-  		{"STR_Crime_17","100","17"},
-  		{"STR_Crime_18","1500","18"},
-  		{"STR_Crime_19","2500","19"},
-  		{"STR_Crime_20","500","20"},
-  		{"STR_Crime_21","500","21"},
-  		{"STR_Crime_22","2000","22"},
-  		{"STR_Crime_23","5000","23"},
-  		{"STR_Crime_24","10000","24"},
-  		{"STR_Crime_25","20000","25"}
-  	};
-
     /* Job-related stuff */
     delivery_points[] = { "dp_1", "dp_2", "dp_3", "dp_4", "dp_5", "dp_6", "dp_7", "dp_8", "dp_9", "dp_10", "dp_11", "dp_12", "dp_13", "dp_14", "dp_15", "dp_15", "dp_16", "dp_17", "dp_18", "dp_19", "dp_20", "dp_21", "dp_22", "dp_23", "dp_24", "dp_25" };
+
+    /* Wanted System related settings */
+    /* crimes[] = {String, Bounty, Code} */
+    crimes[] = {
+        {"STR_Crime_187V","650","187V"},
+        {"STR_Crime_187","2000","187"},
+        {"STR_Crime_901","450","901"},
+        {"STR_Crime_215","200","215"},
+        {"STR_Crime_213","1000","213"},
+        {"STR_Crime_211","100","211"},
+        {"STR_Crime_207","350","207"},
+        {"STR_Crime_207A","200","207A"},
+        {"STR_Crime_390","1500","390"},
+        {"STR_Crime_487","150","487"},
+        {"STR_Crime_488","70","488"},
+        {"STR_Crime_480","100","480"},
+        {"STR_Crime_481","100","481"},
+        {"STR_Crime_482","500","482"},
+        {"STR_Crime_483","950","483"},
+        {"STR_Crime_459","650","459"},
+        {"STR_Crime_666","200","666"},
+        {"STR_Crime_667","4500","667"},
+        {"STR_Crime_668","1500","668"},
+        {"STR_Crime_1","250","1"},
+        {"STR_Crime_2","200","2"},
+        {"STR_Crime_3","150","3"},
+        {"STR_Crime_4","250","4"},
+        {"STR_Crime_5","100","5"},
+        {"STR_Crime_6","80","6"},
+        {"STR_Crime_7","150","7"},
+        {"STR_Crime_8","5000","8"},
+        {"STR_Crime_9","5000","9"},
+        {"STR_Crime_10","15000","10"},
+        {"STR_Crime_11","10000","11"},
+        {"STR_Crime_12","2500","12"},
+        {"STR_Crime_13","1500","13"},
+        {"STR_Crime_14","500","14"},
+        {"STR_Crime_15","2500","15"},
+        {"STR_Crime_16","1500","16"},
+        {"STR_Crime_17","100","17"},
+        {"STR_Crime_18","1500","18"},
+        {"STR_Crime_19","2500","19"},
+        {"STR_Crime_20","500","20"},
+        {"STR_Crime_21","500","21"},
+        {"STR_Crime_22","2000","22"},
+        {"STR_Crime_23","5000","23"},
+        {"STR_Crime_24","10000","24"},
+        {"STR_Crime_25","20000","25"}
+    };
 };
 
 /*
@@ -128,16 +130,15 @@ class Life_Settings {
 		Use with lineIntersectsWith function
 */
 class CfgInteractionModels {
-	class Crate {
-		name = "Crate";
-		models[] = {"supplydrop.p3d","ammobox_f.p3d"};
-	};
-	class House {
-		name = "Houses";
-		models[] = {"i_house_"};
-	};
+    class Crate {
+        name = "Crate";
+        models[] = {"supplydrop.p3d","ammobox_f.p3d"};
+    };
+    class House {
+        name = "Houses";
+        models[] = {"i_house_"};
+    };
 };
-
 
 #include "Config_Clothing.hpp"
 #include "Config_Licenses.hpp"
