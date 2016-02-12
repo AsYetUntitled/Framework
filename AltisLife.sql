21720ce7646036cda6be15259f522af47de19826
Daniel Stuart <daniel.stuart14@gmail.com>
2016-02-12 16:59:03 -0200

    MySQL: Stats: Clean

--- AltisLife.sql
+++ AltisLife.sql
@@ -17,16 +17,17 @@ USE `AltisLife`;
 DELIMITER $$
 --
 -- Procedures
--- Edit arma3 to match a user in MySQL
+-- Edit arma3 to match a user in MySQ
+-- For external databases: Edit localhost to match arma3server IP
 --
 CREATE DEFINER=`arma3`@`localhost` PROCEDURE `resetLifeVehicles`()
 BEGIN
-	UPDATE `vehicles` SET `active`= 0;
+  UPDATE `vehicles` SET `active`= 0;
 END$$
 
 CREATE DEFINER=`arma3`@`localhost` PROCEDURE `deleteDeadVehicles`()
 BEGIN
-	DELETE FROM `vehicles` WHERE `alive` = 0;
+  DELETE FROM `vehicles` WHERE `alive` = 0;
 END$$
 
 CREATE DEFINER=`arma3`@`localhost` PROCEDURE `deleteOldHouses`()
@@ -67,9 +68,9 @@ CREATE TABLE IF NOT EXISTS `players` (
   `civ_gear` text NOT NULL,
   `cop_gear` text NOT NULL,
   `med_gear` text NOT NULL,
-  `civ_stats` varchar(12) NOT NULL DEFAULT '"[100,100]"',
-  `cop_stats` varchar(12) NOT NULL DEFAULT '"[100,100]"',
-  `med_stats` varchar(12) NOT NULL DEFAULT '"[100,100]"',
+  `civ_stats` varchar(11) NOT NULL DEFAULT '"[100,100]"',
+  `cop_stats` varchar(11) NOT NULL DEFAULT '"[100,100]"',
+  `med_stats` varchar(11) NOT NULL DEFAULT '"[100,100]"',
   `arrested` tinyint(1) NOT NULL DEFAULT '0',
   `adminlevel` enum('0','1','2','3','4','5') NOT NULL DEFAULT '0',
   `donatorlvl` enum('0','1','2','3','4','5') NOT NULL DEFAULT '0',
@@ -149,13 +150,13 @@ CREATE TABLE IF NOT EXISTS `gangs` (
 CREATE TABLE IF NOT EXISTS `containers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pid` varchar(32) NOT NULL,
-	`classname` varchar(32) NOT NULL,
+  `classname` varchar(32) NOT NULL,
   `pos` varchar(64) DEFAULT NULL,
-	`inventory` varchar(500) NOT NULL,
+  `inventory` varchar(500) NOT NULL,
   `gear` text NOT NULL,
-	`dir` varchar(64) DEFAULT NULL,
+  `dir` varchar(64) DEFAULT NULL,
   `active` tinyint(1) NOT NULL DEFAULT '0',
-	`owned` tinyint(4) DEFAULT '0',
+  `owned` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`id`,`pid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
