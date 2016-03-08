c6714d48da1fba100fdc1f23dd73a2346671c6e2
Jawshy <Jawshy@users.noreply.github.com>
2016-03-08 15:09:04 +1000

    Added and updated icons again
    ###### Altis_Life.Altis\Config_vItems.hpp
    
    Updated icon file paths to reflect icon changes.
    
    ###### Altis_Life.Altis\icons
    
    - Updated:
    - ico_cannabis.paa
    - ico_heroinUnprocessed.paa
    - ico_cocaineProcessed.paa
    - ico_cocaineUnprocessed.paa
    
    - Added:
    - ico_chickenDrumstick.paa
    - ico_chickenDrumstickRaw.paa
    - ico_lambChop.paa
    - ico_lambChopRaw.paa
    - ico_muttonLeg.paa
    - ico_muttonLegRaw.paa
    - ico_rabbit.paa
    - ico_rabbitRaw.paa
    - ico_turtleRaw.paa
    - ico_turtleSoup.paa
    - ico_wholeChicken.paa
    - ico_wholeChickenRaw.paa
    
    - Removed:
    - ico_catshark.paa
    - ico_cookedMeat.paa
    - ico_meat.paa

--- Altis_Life.Altis/Config_vItems.hpp
+++ Altis_Life.Altis/Config_vItems.hpp
@@ -586,7 +586,7 @@ class VirtualItems {
         sellPrice = 95;
         illegal = false;
         edible = -1;
-        icon = "icons\ico_meat.paa";
+        icon = "icons\ico_rabbitRaw.paa";
     };
 
     class rabbit {
@@ -597,7 +597,7 @@ class VirtualItems {
         sellPrice = 115;
         illegal = false;
         edible = 20;
-        icon = "icons\ico_cookedMeat.paa";
+        icon = "icons\ico_rabbit.paa";
     };
 
     class salema_raw {
@@ -729,7 +729,7 @@ class VirtualItems {
         sellPrice = 500;
         illegal = false;
         edible = 100;
-        icon = "icons\ico_catShark.paa";
+        icon = "icons\ico_cookedFish.paa";
     };
 
     class turtle_raw {
@@ -740,7 +740,7 @@ class VirtualItems {
         sellPrice = 3000;
         illegal = true;
         edible = -1;
-        icon = "icons\ico_meat.paa";
+        icon = "icons\ico_turtleRaw.paa";
     };
 
     class turtle_soup {
@@ -751,7 +751,7 @@ class VirtualItems {
         sellPrice = 750;
         illegal = false;
         edible = 100;
-        icon = "icons\ico_cookedMeat.paa";
+        icon = "icons\ico_turtleSoup.paa";
     };
 
     class hen_raw {
@@ -762,7 +762,7 @@ class VirtualItems {
         sellPrice = 65;
         illegal = false;
         edible = -1;
-        icon = "icons\ico_meat.paa";
+        icon = "icons\ico_wholeChickenRaw.paa";
     };
 
     class hen {
@@ -773,7 +773,7 @@ class VirtualItems {
         sellPrice = 85;
         illegal = false;
         edible = 65;
-        icon = "icons\ico_cookedMeat.paa";
+        icon = "icons\ico_wholeChicken.paa";
     };
 
     class rooster_raw {
@@ -784,7 +784,7 @@ class VirtualItems {
         sellPrice = 65;
         illegal = false;
         edible = -1;
-        icon = "icons\ico_meat.paa";
+        icon = "icons\ico_chickenDrumstickRaw.paa";
     };
 
     class rooster {
@@ -795,7 +795,7 @@ class VirtualItems {
         sellPrice = 85;
         illegal = false;
         edible = 45;
-        icon = "icons\ico_cookedMeat.paa";
+        icon = "icons\ico_chickenDrumstick.paa";
     };
 
     class sheep_raw {
@@ -806,7 +806,7 @@ class VirtualItems {
         sellPrice = 95;
         illegal = false;
         edible = -1;
-        icon = "icons\ico_meat.paa";
+        icon = "icons\ico_lambChopRaw.paa";
     };
 
     class sheep {
@@ -817,7 +817,7 @@ class VirtualItems {
         sellPrice = 115;
         illegal = false;
         edible = 100;
-        icon = "icons\ico_cookedMeat.paa";
+        icon = "icons\ico_lambChop.paa";
     };
 
     class goat_raw {
@@ -828,7 +828,7 @@ class VirtualItems {
         sellPrice = 115;
         illegal = false;
         edible = -1;
-        icon = "icons\ico_meat.paa";
+        icon = "icons\ico_muttonLegRaw.paa";
     };
 
     class goat {
@@ -839,6 +839,6 @@ class VirtualItems {
         sellPrice = 135;
         illegal = false;
         edible = 100;
-        icon = "icons\ico_cookedMeat.paa";
+        icon = "icons\ico_muttonLeg.paa";
     };
 };
