a850265ae61d08236449f0cae1b2b0b73cefa0b2
Daniel Stuart <daniel.stuart14@gmail.com>
2016-03-20 05:17:08 -0300

    SpawnPoints: Better var check method.

--- Altis_Life.Altis/Config_SpawnPoints.hpp
+++ Altis_Life.Altis/Config_SpawnPoints.hpp
@@ -1,107 +1,99 @@
-class CfgSpawnPoints
-{
-    class Civilian
-    {
-        class Kavala 
-        {
+/*
+*    Format:
+*        level: ARRAY (This is for limiting items to certain things)
+*            0: Variable to read from
+*            1: Variable Value Type (SCALAR / BOOL / EQUAL)
+*            2: What to compare to (-1 = Check Disabled)
+*
+*/
+class CfgSpawnPoints {
+    class Civilian {
+        class Kavala {
             displayName = "Kavala";
             spawnMarker = "civ_spawn_1";
             icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-            condition = "true";
+            level[] = { "", "", -1 };
         };
-        
-        class Athira 
-        {
+
+        class Athira {
             displayName = "Athira";
             spawnMarker = "civ_spawn_3";
             icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-            condition = "true";
+            level[] = { "", "", -1 };
         };
-        
-        class Pyrgos 
-        {
+
+        class Pyrgos {
             displayName = "Pyrgos";
             spawnMarker = "civ_spawn_2";
             icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-            condition = "true";
+            level[] = { "", "", -1 };
         };
-        
-        class Sofia 
-        {
+
+        class Sofia {
             displayName = "Sofia";
             spawnMarker = "civ_spawn_4";
             icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-            condition = "true";
+            level[] = { "", "", -1 };
         };
     };
-    
-    class Cop
-    {
-        class Kavala 
-        {
+
+    class Cop {
+        class Kavala {
             displayName = "Kavala HQ";
             spawnMarker = "cop_spawn_1";
             icon = "\a3\ui_f\data\map\MapControl\watertower_ca.paa";
-            condition = "true";
+            level[] = { "", "", -1 };
         };
-        
-        class Athira 
-        {
+
+        class Athira {
             displayName = "Athira HQ";
             spawnMarker = "cop_spawn_3";
             icon = "\a3\ui_f\data\map\MapControl\fuelstation_ca.paa";
-            condition = "true";
+            level[] = { "", "", -1 };
         };
-        
-        class Pyrgos 
-        {
+
+        class Pyrgos {
             displayName = "Pyrgos HQ";
             spawnMarker = "cop_spawn_2";
             icon = "\a3\ui_f\data\map\GroupIcons\badge_rotate_0_gs.paa";
-            condition = "true";
+            level[] = { "", "", -1 };
         };
-        
-        class Air 
-        {
+
+        class Air {
             displayName = "Air HQ";
             spawnMarker = "cop_spawn_4";
             icon = "\a3\ui_f\data\map\Markers\NATO\b_air.paa";
-            condition = "true";
+            level[] = { "life_coplevel", "SCALAR", 2 };
         };
-        
-        class HW 
-        {
+
+        class HW {
             displayName = "HW Patrol";
             spawnMarker = "cop_spawn_5";
             icon = "\a3\ui_f\data\map\GroupIcons\badge_rotate_0_gs.paa";
-            condition = "true";
+            level[] = { "life_coplevel", "SCALAR", 3 };
         };
     };
-    
-    class Medic
-    {
-        class Kavala 
-        {
+
+    class Medic {
+        class Kavala {
             displayName = "Kavala Hospital";
             spawnMarker = "medic_spawn_1";
             icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
-            condition = "true";
+            level[] = { "", "", -1 };
         };
-        
-        class Athira 
-        {
+
+        class Athira {
             displayName = "Athira Regional";
             spawnMarker = "medic_spawn_2";
             icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
-            condition = "true";
+            level[] = { "", "", -1 };
         };
-        
-        class Pyrgos 
-        {
+
+        class Pyrgos {
             displayName = "Pyrgos Hospital";
             spawnMarker = "medic_spawn_3";
             icon = "\a3\ui_f\data\map\MapControl\hospital_ca.paa";
-            condition = "true";
+            level[] = { "", "", -1 };
         };
     };
 };
--- Altis_Life.Altis/dialog/function/fn_spawnPointCfg.sqf
+++ Altis_Life.Altis/dialog/function/fn_spawnPointCfg.sqf
@@ -1,14 +1,15 @@
+#include "..\..\script_macros.hpp"
 /*
 	File: fn_spawnPointCfg.sqf
 	Author: Bryan "Tonic" Boardwine
-	
+
 	Description:
 	Master configuration for available spawn points depending on the units side.
-	
+
 	Return:
 	[Spawn Marker,Spawn Name,Image Path]
 */
-private["_side","_return","_spawnCfg","_curConfig","_name","_condition","_tempConfig"];
+private["_side","_return","_spawnCfg","_curConfig","_name","_level","_levelName","_levelValue","_levelType","_tempConfig","_flag"];
 _side = param [0,civilian,[civilian]];
 
 switch (_side) do {
@@ -23,25 +24,40 @@ _return = [];
 _spawnCfg = missionConfigFile >> "CfgSpawnPoints" >> _side;
 
 for[{_i = 0},{_i < count(_spawnCfg)},{_i = _i + 1}] do {
+    _flag = true;
     _tempConfig = [];
-	_curConfig = (_spawnCfg select _i);
-    _condition = getText(_curConfig >> "condition");
-    if(call compile _condition) then 
-    {
-        _tempConfig pushBack getText(_curConfig >> "spawnMarker");
-        _tempConfig pushBack getText(_curConfig >> "displayName");
-        _tempConfig pushBack getText(_curConfig >> "icon");
-        _return pushBack _tempConfig;
-    };
+		_curConfig = (_spawnCfg select _i);
+    _level = getArray(_curConfig >> "level");
+		_levelName = SEL(_level,0);
+		_levelType = SEL(_level,1);
+		_levelValue = SEL(_level,2);
+
+		if(!(EQUAL(_levelValue,-1))) then {
+			_level = GVAR_MNS _levelName;
+			if(typeName _level == typeName {}) then {_level = FETCH_CONST(_level);};
+			_flag = switch(_levelType) do {
+				case "SCALAR": {_level >= _levelValue};
+				case "BOOL": {_level};
+				case "EQUAL": {EQUAL(_level,_levelValue)};
+				default {false};
+			};
+		};
+
+		if(_flag) then {
+			_tempConfig pushBack getText(_curConfig >> "spawnMarker");
+      _tempConfig pushBack getText(_curConfig >> "displayName");
+      _tempConfig pushBack getText(_curConfig >> "icon");
+      _return pushBack _tempConfig;
+		};
 };
 
-if(count life_houses > 0) then 
+if(count life_houses > 0) then
 {
     {
     _pos = call compile format["%1",_x select 0];
     _house = nearestBuilding _pos;
     _houseName = getText(configFile >> "CfgVehicles" >> (typeOf _house) >> "displayName");
-				
+
     _return pushBack [format["house_%1",_house getVariable "uid"],_houseName,"\a3\ui_f\data\map\MapControl\lighthouse_ca.paa"];
     } foreach life_houses;
 };
