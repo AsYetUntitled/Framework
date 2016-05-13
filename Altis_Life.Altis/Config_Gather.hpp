275ad453289597045291f05f58c89ca85b450aaf
Jesse Schultz <tkcjesse@gmail.com>
2016-05-13 03:13:32 -0500

    Config_Gather Clean Format

--- Altis_Life.Altis/Config_Gather.hpp
+++ Altis_Life.Altis/Config_Gather.hpp
@@ -1,44 +1,38 @@
-class CfgGather
-{
+class CfgGather {
     zoneSize = 30;
-    class Resources
-    {
-        class apple
-        {
+    class Resources {
+        class apple {
             amount = 5;
             zones[] = { "apple_1", "apple_2", "apple_3", "apple_4" };
             item = "";
         };
 
-        class peach
-        {
+        class peach {
             amount = 5;
             zones[] = { "peaches_1", "peaches_2", "peaches_3", "peaches_4" };
             item = "";
         };
 
-        class heroin_unprocessed
-        {
+        class heroin_unprocessed {
             amount = 3;
             zones[] = { "heroin_1" };
             item = "";
         };
 
-        class cocaine_unprocessed
-        {
+        class cocaine_unprocessed {
             amount = 3;
             zones[] = { "cocaine_1" };
             item = "";
         };
 
-        class cannabis
-        {
+        class cannabis {
             amount = 3;
             zones[] = { "weed_1" };
             item = "";
         };
     };
-  /*
+
+/*
 This block can be set using percent,if you want players to mine only one resource ,just leave it as it is.
 Example:
         class copper_unrefined
@@ -60,8 +54,8 @@ Example 2:
     This will give :
     25(±1)% to copper_unrefined;
     70(±1)% to iron_unrefined;
-    5%(±1)% to diamond_uncut; 
-    
+    5%(±1)% to diamond_uncut;
+
                                                          ! Watch Out !
  If percents are used,you MUST put more than 1 resource in the mined parameter
  mined[] = { {"copper_unrefined",0,25} }; NOT OK (But the script will work)
@@ -70,55 +64,51 @@ Example 2:
  mined[] = { "copper_unrefined" }; OK
  mined[] = { {"copper_unrefined",0,35} , { "iron_unrefined" ,35,100 } }; OK
 */
-    class Minerals
-    {
-        class copper_unrefined
-        {
+
+    class Minerals {
+        class copper_unrefined {
             amount = 2;
             zones[] = { "copper_mine" };
             item = "pickaxe";
             mined[] = {"copper_unrefined"};
         };
-        class iron_unrefined
-        {
+
+        class iron_unrefined {
             amount = 2;
             zones[] = { "iron_mine" };
             item = "pickaxe";
             mined[] = { "iron_unrefined" };
         };
 
-        class salt_unrefined
-        {
+        class salt_unrefined {
             amount = 2;
             zones[] = { "salt_mine" };
             item = "pickaxe";
             mined[] = { "salt_unrefined" };
         };
 
-        class sand
-        {
+        class sand {
             amount = 2;
             zones[] = { "sand_mine" };
             item = "pickaxe";
             mined[] = { "sand" };
         };
 
-        class diamond_uncut
-        {
+        class diamond_uncut {
             amount = 2;
             zones[] = { "diamond_mine" };
             item = "pickaxe";
             mined[] = { "diamond_uncut" };
         };
-        class rock
-        {
+
+        class rock {
             amount = 2;
             zones[] = { "rock_quarry" };
             item = "pickaxe";
             mined[] = { "rock" };
         };
-        class oil_unprocessed
-        {
+
+        class oil_unprocessed {
             amount = 2;
             zones[] = { "oil_field_1", "oil_field_2" };
             item = "pickaxe";
