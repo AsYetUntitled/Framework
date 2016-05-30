e5442a23c3079f7be27ec0236db4c9672ec50330
Jesse Schultz <tkcjesse@gmail.com>
2016-05-29 22:21:40 -0500

    Fuel Can Updates
    Uses a verification dialog. Checks for the money, and will remove,
    cancels if you don't have it...
    
    Yeah. Cool. I know right?

--- Altis_Life.Altis/Config_Master.hpp
+++ Altis_Life.Altis/Config_Master.hpp
@@ -58,6 +58,7 @@ class Life_Settings {
     pump_service = false; //Allow users to use pump service on the map. Default = false
     fuel_cost = 80; //Cost of fuel per liter at fuel stations (if not defined for the vehicle already).
     service_chopper = 1000; //Cost to service chopper at chopper service station(Repair/Refuel).
+    fuelCan_refuel = 250; //Cost to refuel an empty fuel canister at the fuel station pumps. (Be wary of your buy/sell prices on fuel cans to prevent exploits...)
 
     /* Gang System Configurations */
     gang_price = 75000; //Gang creation price. --Remember they are persistent so keep it reasonable to avoid millions of gangs.
