998247ced0ef57f86320d4780850b8b786fa4469
Daniel Stuart <daniel.stuart14@gmail.com>
2016-06-06 13:30:25 -0300

    Server: Config: it's a string

--- life_server/config.cpp
+++ life_server/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches {
         weapons[] = {};
         requiredAddons[] = {"A3_Data_F","A3_Soft_F","A3_Soft_F_Offroad_01","A3_Characters_F"};
         fileName = "life_server.pbo";
-        author[]= "Tonic";
+        author = "Tonic";
     };
 };
 
