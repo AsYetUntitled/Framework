d38f899b09f827f9c4a6b15208fd0696941e7bda
BoGuu <vincentbwg@gmail.com>
2016-07-21 02:05:47 +0100

    Add Altis to Housing Config

--- Altis_Life.Altis/config/Config_Housing.hpp
+++ Altis_Life.Altis/config/Config_Housing.hpp
@@ -1,1 +1,62 @@
 
+class Housing {
+  
+    class Altis {
+        
+        class Land_i_House_Big_02_V1_F {
+            price = 1550000;
+            numberCrates = 3;
+        };
+        
+        class Land_i_House_Big_02_V2_F : Land_i_House_Big_02_V1_F{};
+        class Land_i_House_Big_02_V3_F : Land_i_House_Big_02_V1_F{};
+
+        class Land_i_House_Big_01_V1_F {
+            price = 2200000;
+            numberCrates = 4;
+        };
+
+        class Land_i_House_Big_01_V2_F : Land_i_House_Big_01_V1_F{};
+        class Land_i_House_Big_01_V3_F : Land_i_House_Big_01_V1_F{};
+
+        class Land_i_Garage_V1_F {
+            price = 500000;
+            numberCrates = 0;            
+        };
+
+        class Land_i_Garage_V2_F : Land_i_Garage_V1_F{};
+
+        class Land_i_House_Small_01_V1_F {
+            price = 1050000;
+            numberCrates = 2;             
+        };
+
+        class Land_i_House_Small_01_V2_F : Land_i_House_Small_01_V1_F{};
+        class Land_i_House_Small_01_V3_F : Land_i_House_Small_01_V1_F{};
+
+        class Land_i_House_Small_02_V1_F {
+            price = 1000500;
+            numberCrates = 2;             
+        };
+
+        class Land_i_House_Small_02_V2_F : Land_i_House_Small_02_V1_F{};
+        class Land_i_House_Small_02_V3_F : Land_i_House_Small_02_V1_F{};
+
+        class Land_i_House_Small_03_V1_F {
+            price = 1250000;
+            numberCrates = 3;             
+        };
+
+        class Land_i_Stone_HouseSmall_V2_F {
+            price = 750000;
+            numberCrates = 1;             
+        };
+
+        class Land_i_Stone_HouseSmall_V1_F : Land_i_Stone_HouseSmall_V2_F{};
+        class Land_i_Stone_HouseSmall_V3_F : Land_i_Stone_HouseSmall_V2_F{};
+  
+    };
+};
+  
+  
+  
