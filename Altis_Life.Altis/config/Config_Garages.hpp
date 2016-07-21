4753c3e59bee547800e651f3da2e7b241421c67c
BoGuu <vincentbwg@gmail.com>
2016-07-21 02:57:41 +0100

    Undefined base class - derp

--- Altis_Life.Altis/config/Config_Garages.hpp
+++ Altis_Life.Altis/config/Config_Garages.hpp
@@ -10,8 +10,8 @@ class Garages {
             price = 500000;
         };
 
-        class Land_i_House_Big_02_V2_F : Land_i_House_Big_02_V1_F{};
-        class Land_i_House_Big_02_V3_F : Land_i_House_Big_02_V1_F{};
+        class Land_i_House_Big_02_V2_F : Land_i_Garage_V1_F{};
+        class Land_i_House_Big_02_V3_F : Land_i_Garage_V1_F{};
 
     };
 
