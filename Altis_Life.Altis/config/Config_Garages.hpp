d60136dca9bbbb489c535a8dc1c5e06fbafd1623
BoGuu <vincentbwg@gmail.com>
2016-07-21 03:46:25 +0100

    Add spawnPos and spawnDir to garages

--- Altis_Life.Altis/config/Config_Garages.hpp
+++ Altis_Life.Altis/config/Config_Garages.hpp
@@ -1,5 +1,8 @@
 /*
     Anything defined in Config_Garages is strictly just a garage, it will not act like a house.
+    price - Cost of the garage
+    garageSpawnPos[] - worldToModel position to spawn vehicles
+    garageSpawnDir - Offset to garage direction to spawn at (+-90 etc). It will be used in the manner: getDir _garage + spawnDir
 */
 
 class Garages {
@@ -8,6 +11,8 @@ class Garages {
 
         class Land_i_Garage_V1_F {
             price = 500000;
+            garageSpawnPos[] = {-11.5,0,0};
+            garageSpawnDir = -90;
         };
 
         class Land_i_Garage_V2_F : Land_i_Garage_V1_F{};
