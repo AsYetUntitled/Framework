edb2086f533c7df35ba3f18044aee069c1f8b3dc
BoGuu <vincentbwg@gmail.com>
2016-07-21 02:34:49 +0100

    Add garage utility - housing config

--- Altis_Life.Altis/config/Config_Housing.hpp
+++ Altis_Life.Altis/config/Config_Housing.hpp
@@ -5,6 +5,8 @@ class Housing {
         class Land_i_House_Big_02_V1_F {
             price = 1550000;
             numberCrates = 3;
+            canGarage = false;
+            spawnPoint[] = {};
         };
         
         class Land_i_House_Big_02_V2_F : Land_i_House_Big_02_V1_F{};
@@ -13,6 +15,8 @@ class Housing {
         class Land_i_House_Big_01_V1_F {
             price = 2200000;
             numberCrates = 4;
+            canGarage = false;
+            spawnPoint[] = {};
         };
 
         class Land_i_House_Big_01_V2_F : Land_i_House_Big_01_V1_F{};
@@ -20,14 +24,18 @@ class Housing {
 
         class Land_i_Garage_V1_F {
             price = 500000;
-            numberCrates = 0;            
+            numberCrates = 0;
+            canGarage = false;
+            spawnPoint[] = {};
         };
 
         class Land_i_Garage_V2_F : Land_i_Garage_V1_F{};
 
         class Land_i_House_Small_01_V1_F {
             price = 1050000;
-            numberCrates = 2;             
+            numberCrates = 2;
+            canGarage = false;
+            spawnPoint[] = {};
         };
 
         class Land_i_House_Small_01_V2_F : Land_i_House_Small_01_V1_F{};
@@ -35,7 +43,9 @@ class Housing {
 
         class Land_i_House_Small_02_V1_F {
             price = 1000500;
-            numberCrates = 2;             
+            numberCrates = 2;
+            canGarage = false;
+            spawnPoint[] = {};
         };
 
         class Land_i_House_Small_02_V2_F : Land_i_House_Small_02_V1_F{};
@@ -43,12 +53,16 @@ class Housing {
 
         class Land_i_House_Small_03_V1_F {
             price = 1250000;
-            numberCrates = 3;             
+            numberCrates = 3;
+            canGarage = false;
+            spawnPoint[] = {};
         };
 
         class Land_i_Stone_HouseSmall_V2_F {
             price = 750000;
-            numberCrates = 1;             
+            numberCrates = 1;
+            canGarage = false;
+            spawnPoint[] = {};
         };
 
         class Land_i_Stone_HouseSmall_V1_F : Land_i_Stone_HouseSmall_V2_F{};
