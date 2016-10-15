93cd55c496174d394e98feeb2632afb7d9e4e399
Simon Arledal <arledal.simon@gmail.com>
2016-10-15 18:16:36 +0200

    Revert "Remove forcewalk toggle option in master"
    This reverts commit 3a76e8ee3527548f1059f7e90ad1032121f6bd2e.

--- Altis_Life.Altis/config/Config_Master.hpp
+++ Altis_Life.Altis/config/Config_Master.hpp
@@ -95,7 +95,8 @@ class Life_Settings {
     /* Jail System Configurations */
     jail_seize_vItems[] = { "spikeStrip","lockpick","goldbar","blastingcharge","boltcutter","defusekit","heroin_unprocessed","heroin_processed","cannabis","marijuana","cocaine_unprocessed","cocaine_processed","turtle_raw" }; //Define VIRTUAL items you want to be removed from players upon jailing here. Use "jail_seize_inventory" for Arma inventory items.
     jail_seize_inventory = false; //Set to true to run the cop seize script on inmates. False will remove only weapons and magazines otherwise. (Basically used in case cops forget to seize items). [See Lines 106-111 below]
-    sendtoJail_locations[] = { "police_hq_1", "police_hq_2", "cop_spawn_3", "cop_spawn_5", "Correctional_Facility" }; //Enter the variableName from the mission.sqm here to allow cops to send a person to jail at these locations
+    sendtoJail_locations[] = { "police_hq_1", "police_hq_2", "cop_spawn_3", "cop_spawn_5", "Correctional_Facility" }; //Enter the variableName from the mission.sqm here to allow cops to send a person to jail at these locations.
+    jail_forceWalk = true; // Disable running/jumping in jail to avoid glitchers
     jail_helicopter_only = true; // If enabled (true) this will teleport players back into the jail if they're not in a helicopter to avoid jailglitches
 
     /* Medical System Configurations */
