e9ff24ab2f333db46c34e2843243ea5ba60acd03
BoGuu <boguug@gmail.com>
2016-10-18 20:42:38 +0100

    Update Config_Master.hpp

--- Altis_Life.Altis/config/Config_Master.hpp
+++ Altis_Life.Altis/config/Config_Master.hpp
@@ -40,8 +40,8 @@ class Life_Settings {
     global_ATM = true; //Allow users to access any ATM on the map (Marked & Unmarked).
     noatm_timer = 10; //Time in minutes that players cannot deposit money after selling stolen gold.
     minimum_cops = 5; //Minimum cops required online to rob the Federal Reserve
-	
-	/*Death settings*/
+
+    /*Death settings*/
     drop_weapons_onDeath = false; //Set true to enable weapon dropping on death. False (default) will delete player weapons on death, allowing them to be revived with them instead
 
     /* Basic System Configurations */
