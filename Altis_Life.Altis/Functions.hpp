3c63ac450bf56d0e7952554d3d3038783c144579
Trinity <trinitynet@users.noreply.github.com>
2016-12-28 14:31:28 +0000

    adding back SPY-GLASS fn's

--- Altis_Life.Altis/Functions.hpp
+++ Altis_Life.Altis/Functions.hpp
@@ -11,6 +11,18 @@ class Socket_Reciever {
     };
 };
 
+class SpyGlass
+{
+	tag = "SPY";
+	class Functions
+	{
+		file = "SpyGlass";
+		class cookieJar{};
+		class notifyAdmins{};
+		class observe{};
+	};
+};
+
 class Life_Client_Core {
     tag = "life";
 
