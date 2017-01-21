adb9828a5db7832506ee01804c3890c2980e9a3d
Jawsh <Jawshy@users.noreply.github.com>
2017-01-21 15:25:22 +1000

    Config_Vehicles: cop_ship: Added: RHIB
    Added the RHIB to the police boat shop.

--- Altis_Life.Altis/config/Config_Vehicles.hpp
+++ Altis_Life.Altis/config/Config_Vehicles.hpp
@@ -142,6 +142,7 @@ class CarShops {
         vehicles[] = {
             { "B_Boat_Transport_01_F", "" },
             { "C_Boat_Civil_01_police_F", "" },
+            { "C_Boat_Transport_02_F", "" }, //Apex DLC
             { "B_Boat_Armed_01_minigun_F", "call life_coplevel >= 3" },
             { "B_SDV_01_F", "" }
         };
