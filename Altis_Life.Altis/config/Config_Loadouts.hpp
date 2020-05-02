160a64cff8da5be39cb944be6b0cc2bcc3676de6
Casperento <rafalzdev16@gmail.com>
2020-05-02 14:02:49 -0300

    first approach for new loadouts

--- Altis_Life.Altis/config/Config_Loadouts.hpp
+++ Altis_Life.Altis/config/Config_Loadouts.hpp
@@ -2,77 +2,118 @@
     class PLAYERSIDE { // PLAYERSIDE can be: WEST (for cops), CIV (for civ/reb), GUER (for medics), EAST (for opfor)
         // NOTES:
         //     empty array means that nothing will be add on players
-        //     if you put more than a uniform on the CIV's class, they will be selected randonly,
+        //     CIV's loadout are selected randonly if he is not in jail,
         //         otherwise, for the other teams, player will get the uniform related to his level
 
-        itemType[] = { // itemType can be: uniform, vest, backpack, weapon, items or linkedItems
-            { "classname", "conditions" }
+        class lvl_X : lvl_base { // where X is a level of the selected team and inherits from lvl_base
+            uniformClass = "";
+            backpack = "";
+            linkedItems[] = {};
+            weapons[] = {};
+            items[] = {};
+            magazines[] = {};
         };
     };
 */
+class lvl_base {
+    uniformClass = "";
+    backpack = "";
+    linkedItems[] = {"ItemMap","ItemCompass","ItemWatch"};
+    weapons[] = {};
+    items[] = {};
+    magazines[] = {};
+};
+
 class Loadouts {
-    // COP
-    class WEST {
-        uniform[] = {
-            {"U_Rangemaster", "call life_copLevel >= 0"}
+    // CIV
+    class CIV {
+        class lvl_arrested : lvl_base {
+            uniformClass = "U_C_WorkerCoveralls"; // Arrested player's uniform
+            linkedItems[] = {};
         };
-        vest[] = {
-            {"V_Rangemaster_belt", "call life_copLevel >= 0"}
+        class lvl_1 : lvl_base {
+            uniformClass = "U_C_Poloshirt_blue";
         };
-        backpack[] = {};
-        weapon[] = {
-            {"hgun_P07_snds_F", "call life_copLevel >= 0"}
+        class lvl_2 : lvl_base {
+            uniformClass = "U_C_Poloshirt_burgundy";
         };
-        mags[] = {
-            {"16Rnd_9x21_Mag", 6, "call life_copLevel >= 0"}
+        class lvl_3 : lvl_base {
+            uniformClass = "U_C_Poloshirt_stripped";
         };
-        items[] = {};
-        linkedItems[] = {
-            {"ItemMap", "call life_copLevel >= 0"},
-            {"ItemCompass", "call life_copLevel >= 0"},
-            {"ItemWatch", "call life_copLevel >= 0"}
+        class lvl_4 : lvl_base {
+            uniformClass = "U_C_Poloshirt_tricolour";
+        };
+        class lvl_5 : lvl_base {
+            uniformClass = "U_C_Poloshirt_salmon";
+        };
+        class lvl_6 : lvl_base {
+            uniformClass = "U_C_Poloshirt_redwhite";
+        };
+        class lvl_7 : lvl_base {
+            uniformClass = "U_C_Commoner1_1";
         };
     };
 
-    // CIV
-    class CIV {
-        uniform[] = {
-            {"U_C_Poloshirt_blue", "!life_is_arrested"},
-            {"U_C_Poloshirt_burgundy", "!life_is_arrested"},
-            {"U_C_Poloshirt_stripped", "!life_is_arrested"},
-            {"U_C_Poloshirt_tricolour", "!life_is_arrested"},
-            {"U_C_Poloshirt_salmon", "!life_is_arrested"},
-            {"U_C_Poloshirt_redwhite", "!life_is_arrested"},
-            {"U_C_Commoner1_1", "!life_is_arrested"}
+    // COP
+    class WEST {
+        class lvl_1 : lvl_base {
+            uniformClass = "U_Rangemaster";
+            weapons[] = {"hgun_P07_snds_F"};
+            magazines[] = {"16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag"};
         };
-        vest[] = {};
-        backpack[] = {};
-        weapon[] = {};
-        mags[] = {};
-        items[] = {};
-        linkedItems[] = {
-            {"ItemMap", ""},
-            {"ItemCompass", ""},
-            {"ItemWatch", ""}
+        class lvl_2 : lvl_base {
+            uniformClass = "U_Rangemaster";
+            weapons[] = {"hgun_P07_snds_F"};
+            magazines[] = {"16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag"};
+        };
+        class lvl_3 : lvl_base {
+            uniformClass = "U_Rangemaster";
+            weapons[] = {"hgun_P07_snds_F"};
+            magazines[] = {"16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag"};
+        };
+        class lvl_4 : lvl_base {
+            uniformClass = "U_Rangemaster";
+            weapons[] = {"hgun_P07_snds_F"};
+            magazines[] = {"16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag"};
+        };
+        class lvl_5 : lvl_base {
+            uniformClass = "U_Rangemaster";
+            weapons[] = {"hgun_P07_snds_F"};
+            magazines[] = {"16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag"};
+        };
+        class lvl_6 : lvl_base {
+            uniformClass = "U_Rangemaster";
+            weapons[] = {"hgun_P07_snds_F"};
+            magazines[] = {"16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag"};
+        };
+        class lvl_7 : lvl_base {
+            uniformClass = "U_Rangemaster";
+            weapons[] = {"hgun_P07_snds_F"};
+            magazines[] = {"16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag"};
         };
     };
 
     // MED
     class GUER {
-        uniform[] = {
-            {"U_Rangemaster", "call life_medicLevel >= 1"}
+        class lvl_1 : lvl_base {
+            uniformClass = "U_Rangemaster";
+            items[] = {"FirstAidKit"};
         };
-        vest[] = {};
-        backpack[] = {};
-        weapon[] = {};
-        mags[] = {};
-        items[] = {
-            {"FirstAidKit", 2, "call life_medicLevel >= 1"}
+        class lvl_2 : lvl_base {
+            uniformClass = "U_Rangemaster";
+            items[] = {"FirstAidKit"};
         };
-        linkedItems[] = {
-            {"ItemMap", "call life_medicLevel >= 1"},
-            {"ItemCompass", "call life_medicLevel >= 1"},
-            {"ItemWatch", "call life_medicLevel >= 1"}
+        class lvl_3 : lvl_base {
+            uniformClass = "U_Rangemaster";
+            items[] = {"FirstAidKit"};
+        };
+        class lvl_4 : lvl_base {
+            uniformClass = "U_Rangemaster";
+            items[] = {"FirstAidKit"};
+        };
+        class lvl_5 : lvl_base {
+            uniformClass = "U_Rangemaster";
+            items[] = {"FirstAidKit"};
         };
     };
 };
